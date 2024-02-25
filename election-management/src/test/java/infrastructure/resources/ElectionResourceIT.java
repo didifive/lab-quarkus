7c8bc5e672a9b75081f70f28ad50010d0852e046
Luis Zancanela <luisczdidi@gmail.com>
2024-02-25 15:38:45 -0300

    cicd: bump version election-management:1.0.16

---
+++ election-management/src/test/java/infrastructure/resources/ElectionResourceIT.java
@@ -0,0 +1,36 @@
+package infrastructure.resources;
+
+import api.ElectionApi;
+import api.dto.in.CreateCandidate;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import org.instancio.Instancio;
+import org.jboss.resteasy.reactive.RestResponse;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.MediaType;
+
+import static io.restassured.RestAssured.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@QuarkusTest
+@TestHTTPEndpoint(ElectionResource.class)
+class ElectionResourceTest {
+
+    @InjectMock
+    ElectionApi api;
+
+    @Test
+    void submit() {
+
+        given().contentType(MediaType.APPLICATION_JSON)
+                .when().post()
+                .then().statusCode(RestResponse.StatusCode.CREATED);
+
+        verify(api).submit();
+        verifyNoMoreInteractions(api);
+    }
+
+}
